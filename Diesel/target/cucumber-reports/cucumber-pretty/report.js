2ab91f832f6d7e556b76376a8bd88c00dd60c383
Nitish <54052518+Nitish@users.noreply.github.com>
2021-01-15 18:03:28 +0530

    tee
    tets

---
+++ Diesel/target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,392 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Diesel.feature");
+formatter.feature({
+  "line": 1,
+  "name": "R-PACMango.com CRM E-Coomerece Feature Main Finish..",
+  "description": "",
+  "id": "r-pacmango.com-crm-e-coomerece-feature-main-finish..",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 93,
+  "name": "Normal PO order",
+  "description": "",
+  "id": "r-pacmango.com-crm-e-coomerece-feature-main-finish..;normal-po-order",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 92,
+      "name": "@testcaseN001"
+    }
+  ]
+});
+formatter.step({
+  "line": 95,
+  "name": "Navigate user to Login Page \"http://devrtrac.r-pac.com/\" and \"Chrome\"",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 96,
+  "name": "title of login page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 97,
+  "name": "user enters \"Admin@diesel.com\" and \"Lousie#786\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 98,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 99,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 100,
+  "name": "Search Release PO page with CPO ID \"CK_IN20201974\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 101,
+  "name": "Search Vendor ID \"HK000227\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 102,
+  "name": "click on PO \"CK_IN20201974\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 103,
+  "name": "AddToCart with Create Label",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 104,
+  "name": "goto checkout page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 105,
+  "name": "Verify All PO data with db",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 106,
+  "name": "Ship To,selct Production location Vendor PO, Requird Shipment Date",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 107,
+  "name": "Confirm Order",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 108,
+  "name": "Print Confirmation and done",
+  "keyword": "Then "
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 109,
+      "value": "#Then Goto Report\u003eOrder Status"
+    },
+    {
+      "line": 110,
+      "value": "#Then Goto Report\u003eOrder Status-\u003eCancel Order"
+    }
+  ],
+  "line": 111,
+  "name": "Logout",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 112,
+  "name": "Close Browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 113,
+  "name": "",
+  "description": "",
+  "id": "r-pacmango.com-crm-e-coomerece-feature-main-finish..;normal-po-order;",
+  "rows": [
+    {
+      "cells": [
+        "items",
+        "udateitems"
+      ],
+      "line": 114,
+      "id": "r-pacmango.com-crm-e-coomerece-feature-main-finish..;normal-po-order;;1"
+    },
+    {
+      "cells": [
+        "1111",
+        "2222"
+      ],
+      "line": 115,
+      "id": "r-pacmango.com-crm-e-coomerece-feature-main-finish..;normal-po-order;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 115,
+  "name": "Normal PO order",
+  "description": "",
+  "id": "r-pacmango.com-crm-e-coomerece-feature-main-finish..;normal-po-order;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 92,
+      "name": "@testcaseN001"
+    }
+  ]
+});
+formatter.step({
+  "line": 95,
+  "name": "Navigate user to Login Page \"http://devrtrac.r-pac.com/\" and \"Chrome\"",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 96,
+  "name": "title of login page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 97,
+  "name": "user enters \"Admin@diesel.com\" and \"Lousie#786\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 98,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 99,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 100,
+  "name": "Search Release PO page with CPO ID \"CK_IN20201974\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 101,
+  "name": "Search Vendor ID \"HK000227\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 102,
+  "name": "click on PO \"CK_IN20201974\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 103,
+  "name": "AddToCart with Create Label",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 104,
+  "name": "goto checkout page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 105,
+  "name": "Verify All PO data with db",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 106,
+  "name": "Ship To,selct Production location Vendor PO, Requird Shipment Date",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 107,
+  "name": "Confirm Order",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 108,
+  "name": "Print Confirmation and done",
+  "keyword": "Then "
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 109,
+      "value": "#Then Goto Report\u003eOrder Status"
+    },
+    {
+      "line": 110,
+      "value": "#Then Goto Report\u003eOrder Status-\u003eCancel Order"
+    }
+  ],
+  "line": 111,
+  "name": "Logout",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 112,
+  "name": "Close Browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://devrtrac.r-pac.com/",
+      "offset": 29
+    },
+    {
+      "val": "Chrome",
+      "offset": 62
+    }
+  ],
+  "location": "LoginStepDefinition.navigateUser(String,String)"
+});
+formatter.result({
+  "duration": 16182466900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
+});
+formatter.result({
+  "duration": 4921900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Admin@diesel.com",
+      "offset": 13
+    },
+    {
+      "val": "Lousie#786",
+      "offset": 36
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 21812811901,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 79100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_hopme_page()"
+});
+formatter.result({
+  "duration": 70001,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "CK_IN20201974",
+      "offset": 36
+    }
+  ],
+  "location": "LoginStepDefinition.Release_PO_page(String)"
+});
+formatter.result({
+  "duration": 31902413900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "HK000227",
+      "offset": 18
+    }
+  ],
+  "location": "LoginStepDefinition.SuppilerSearch(String)"
+});
+formatter.result({
+  "duration": 3122089400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "CK_IN20201974",
+      "offset": 13
+    }
+  ],
+  "location": "LoginStepDefinition.POClickb(String)"
+});
+formatter.result({
+  "duration": 10751556500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.getAddToCartCareLabel()"
+});
+formatter.result({
+  "duration": 13205373100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.goto_checkout()"
+});
+formatter.result({
+  "duration": 2978448300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.verifyAllPOData()"
+});
+formatter.result({
+  "duration": 6363594100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.select_checkout_details()"
+});
+formatter.result({
+  "duration": 7838821001,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.confirm_order()"
+});
+formatter.result({
+  "duration": 30873235700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.print_confirmation_done()"
+});
+formatter.result({
+  "duration": 300600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.logout()"
+});
+formatter.result({
+  "duration": 16910405299,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "duration": 3763735999,
+  "status": "passed"
+});
+});
